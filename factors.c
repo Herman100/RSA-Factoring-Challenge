c64cdb8c2a92e8519ad50e5e5cebf8ce90766774
Herman100 <hermankwamebour30@gmail.com>
2023-05-30 16:29:07 +0000

    code changed

--- factors.c
+++ factors.c
@@ -2,71 +2,53 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
- * factors - Prints the prime factors of numbers from a file.
- * @filename: The name of the file containing numbers.
- *
- * Description: This function reads numbers from
- * a file and prints their prime factors.
- * It opens the file, reads each line,
- * converts the line to an integer, and finds the
- * prime factors of the number.
- * The prime factors are printed in the format "number=i*j",
- * where i and j are the prime factors of the number.
- */
-void factors(const char *filename)
-{
-	FILE *file = fopen(filename, "r");
 
-	if (file == NULL)
-	{
-		printf("Error: Cannot open file %s\n", filename);
-		return;
-	}
-
-	char line[100];
-
-	while (fgets(line, sizeof(line), file) != NULL)
-
-	{
-		int n = atoi(line);
-
-		for (int i = 2; i <= sqrt(n); i++)
-		{
-			if (n % i == 0)
-			{
-				printf("%d=%d*%d\n", n, i, n / i);
-				break;
-			}
-		}
-	}
-
-	fclose(file);
-}
 
 /**
  * main - Entry point of the program.
  * @argc: The number of command-line arguments.
  * @argv: An array of strings containing the command-line arguments.
  *
- * Description: This function serves as the entry
- * point of the program. It checks
- * if the correct number of command-line arguments is provided, and if not, it
- * displays the correct usage.
- * It then calls the factors function with the file name
- * provided as the command-line argument.
+ * Description: This program takes a filename as a command-line argument and
+ * reads each line of the file. For each line, it finds the factors of the
+ * number and prints them in the format "n=i*j" where n is the number and i, j
+ * are the factors.
  *
- * Return: 0 on success, 1 on failure.
+ * Return: 0 on success, 1 on error.
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		printf("Usage: %s <file>\n", argv[0]);
+		printf("Usage: factors <file>\n");
 		return (1);
 	}
 
-	factors(argv[1]);
+	FILE *file = fopen(argv[1], "r");
 
+	if (file == NULL)
+	{
+		printf("Error: could not open file %s\n", argv[1]);
+		return (1);
+	}
+
+	char line[1024];
+
+	while (fgets(line, sizeof(line), file))
+	{
+		long n = atol(line);
+
+		for (long i = 2; i <= sqrt(n); i++)
+		{
+			if (n % i == 0)
+			{
+				printf("%ld=%ld*%ld\n", n, i, n / i);
+				break;
+			}
+		}
+	}
+
+	fclose(file);
 	return (0);
 }
